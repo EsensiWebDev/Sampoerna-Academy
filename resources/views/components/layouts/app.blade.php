5dde341eb76355b2a8db2cdf4efbcc7b7586f2aa
Feri Winarta <feri@suiten.id>
2024-10-31 20:54:06 +0700

    add navbar component with localization

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,11 +2,18 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="icon" type="image/webp" sizes="1131x1345" href="{{ asset("assets/img/Icon/Five%20Icon.webp") }}">
+        <link rel="stylesheet" href="{{ asset("assets/bootstrap/css/bootstrap.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/styles.min.css") }}">
+        <title>{{ config("app.name") }}</title>
     </head>
-    <body>
+    <body class="bg-dark">
+        @include("components.navbar")
         {{ $slot }}
+
+
+        <script src="{{ asset("assets/bootstrap/js/bootstrap.min.js") }}"></script>
+        <script src="{{ asset("assets/js/script.min.js") }}"></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Livewire\Test;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", Test::class);
+
+
+Route::get('language/{locale}', function ($locale) {
+    app()->setLocale($locale);
+    session()->put('locale', $locale);
+    return redirect()->back();
 });
