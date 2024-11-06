29eef35bf9fd9bbb76f621db75ea247b0d273cee
Feri Winarta <feri@suiten.id>
2024-11-06 19:53:11 +0700

    ourschools

--- resources/views/livewire/pages/ourschools.blade.php
+++ resources/views/livewire/pages/ourschools.blade.php
@@ -1,7 +1,7 @@
 <section id="our-schools">
     <section style="margin-bottom: 200px;">
         <div class="d-flex align-items-end" style="height: 600px;background:
-        url('/assets/img/Carousel/Our%20School%20Hero.jpg') center / cover no-repeat;">
+        url('/assets/img/Carousel/Our%20School%20Hero.webp') center / cover no-repeat;">
             <div class="container" style="padding-bottom: 64px;">
                 <div class="row">
                     <div class="col-md-12">
@@ -14,7 +14,7 @@
     <section>
         <div class="container" style="padding-left: 24px;padding-right: 24px;">
             <div class="row">
-                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(5).jpg"></div>
+                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(5).webp"></div>
                 <div class="col-md-6 d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center justify-content-xxl-start" style="background: #E4E4EB;padding-left: 20px;padding-right: 20px;">
                     <div style="background: transparent;padding-left: 0px;padding-right: 0px;padding-top: 16px;padding-bottom: 16px;">
                         <h1 class="fw-semibold" style="font-family: Campton;color: var(--bs-black);">{{ __("L’Avenue School") }}</h1>
@@ -25,7 +25,7 @@
                                 <path d="M15 11C15 12.6569 13.6569 14 12 14C10.3431 14 9 12.6569 9 11C9 9.34315 10.3431 8 12 8C13.6569 8 15 9.34315 15 11Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                             </svg>
                             <p>Jl. Raya Pasar Minggu Kav. 16,<br>Pancoran, Jakarta 12780</p>
-                        </div><a href="ourschools/lavenueschool.html" style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
+                        </div><a href="ourschools/lavenue" wire:navigate.hover style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2929 5.29289C12.6834 4.90237 13.3166 4.90237 13.7071 5.29289L17.7071 9.29289C18.0976 9.68342 18.0976 10.3166 17.7071 10.7071L13.7071 14.7071C13.3166 15.0976 12.6834 15.0976 12.2929 14.7071C11.9024 14.3166 11.9024 13.6834 12.2929 13.2929L14.5858 11H3C2.44772 11 2 10.5523 2 10C2 9.44772 2.44772 9 3 9H14.5858L12.2929 6.70711C11.9024 6.31658 11.9024 5.68342 12.2929 5.29289Z" fill="currentColor"></path>
                             </svg></a>
                     </div>
@@ -44,17 +44,17 @@
                                 <path d="M15 11C15 12.6569 13.6569 14 12 14C10.3431 14 9 12.6569 9 11C9 9.34315 10.3431 8 12 8C13.6569 8 15 9.34315 15 11Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                             </svg>
                             <p>Jl. Raya Serpong BSD CBD Lot II-2,<br>Lengkong Gudang, Serpong, Tangerang Selatan</p>
-                        </div><a href="ourschools/bsdschool.html" style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
+                        </div><a href="ourschools/bsd" wire:navigate.hover style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2929 5.29289C12.6834 4.90237 13.3166 4.90237 13.7071 5.29289L17.7071 9.29289C18.0976 9.68342 18.0976 10.3166 17.7071 10.7071L13.7071 14.7071C13.3166 15.0976 12.6834 15.0976 12.2929 14.7071C11.9024 14.3166 11.9024 13.6834 12.2929 13.2929L14.5858 11H3C2.44772 11 2 10.5523 2 10C2 9.44772 2.44772 9 3 9H14.5858L12.2929 6.70711C11.9024 6.31658 11.9024 5.68342 12.2929 5.29289Z" fill="currentColor"></path>
                             </svg></a>
                     </div>
                 </div>
-                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(6).jpg"></div>
+                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(6).webp"></div>
             </div>
         </div>
         <div class="container" style="padding-left: 24px;padding-right: 24px;margin-top: 72px;">
             <div class="row">
-                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(7).jpg"></div>
+                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(7).webp"></div>
                 <div class="col-md-6 d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center justify-content-xxl-start" style="background: #E4E4EB;padding-left: 20px;padding-right: 20px;">
                     <div style="background: transparent;padding-left: 0px;padding-right: 0px;padding-top: 16px;padding-bottom: 16px;">
                         <h1 class="fw-semibold" style="font-family: Campton;color: var(--bs-black);">{{ __("Sentul School") }}</h1>
@@ -65,7 +65,7 @@
                                 <path d="M15 11C15 12.6569 13.6569 14 12 14C10.3431 14 9 12.6569 9 11C9 9.34315 10.3431 8 12 8C13.6569 8 15 9.34315 15 11Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                             </svg>
                             <p>Jl. MH. Thamrin, Cijayanti,<br>Sentul Alaya, Sentul City, Bogor</p>
-                        </div><a href="ourschools/sentulschool.html" style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
+                        </div><a href="ourschools/sentul" wire:navigate.hover style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2929 5.29289C12.6834 4.90237 13.3166 4.90237 13.7071 5.29289L17.7071 9.29289C18.0976 9.68342 18.0976 10.3166 17.7071 10.7071L13.7071 14.7071C13.3166 15.0976 12.6834 15.0976 12.2929 14.7071C11.9024 14.3166 11.9024 13.6834 12.2929 13.2929L14.5858 11H3C2.44772 11 2 10.5523 2 10C2 9.44772 2.44772 9 3 9H14.5858L12.2929 6.70711C11.9024 6.31658 11.9024 5.68342 12.2929 5.29289Z" fill="currentColor"></path>
                             </svg></a>
                     </div>
@@ -92,17 +92,17 @@
                                 </svg>
                                 <p>Jl. Grand Pakuwon,<br>Banjar Sugihan, Tandes</p>
                             </div>
-                        </div><a href="ourschools/surabayaschool.html" style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
+                        </div><a href="ourschools/surabaya" wire:navigate.hover style="color: #292F63;font-family: Campton;">{{ __("More Info") }}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2929 5.29289C12.6834 4.90237 13.3166 4.90237 13.7071 5.29289L17.7071 9.29289C18.0976 9.68342 18.0976 10.3166 17.7071 10.7071L13.7071 14.7071C13.3166 15.0976 12.6834 15.0976 12.2929 14.7071C11.9024 14.3166 11.9024 13.6834 12.2929 13.2929L14.5858 11H3C2.44772 11 2 10.5523 2 10C2 9.44772 2.44772 9 3 9H14.5858L12.2929 6.70711C11.9024 6.31658 11.9024 5.68342 12.2929 5.29289Z" fill="currentColor"></path>
                             </svg></a>
                     </div>
                 </div>
-                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(8).jpg"></div>
+                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(8).webp"></div>
             </div>
         </div>
         <div class="container" style="margin-top: 72px;padding-left: 24px;padding-right: 24px;">
             <div class="row">
-                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(9).jpg"></div>
+                <div class="col-md-6 offset-xxl-0" style="padding-left: 0px;padding-right: 0px;background: #E4E4EB;"><img class="img-fluid" src="/assets/img/Banner/Background%20OVerlay%20(9).webp"></div>
                 <div class="col-md-6 d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center justify-content-xxl-start" style="background: #E4E4EB;padding-left: 20px;padding-right: 20px;">
                     <div style="background: transparent;padding-left: 0px;padding-right: 0px;padding-top: 16px;padding-bottom: 16px;">
                         <h1 class="fw-semibold" style="font-family: Campton;color: var(--bs-black);">{{ __("Medan School") }}</h1>
@@ -121,7 +121,7 @@
                                 </svg>
                                 <p>Jl. Jamin Ginting,<br>Kompleks Citra Garden, Medan</p>
                             </div>
-                        </div><a href="ourschools/medanschool.html" style="color: #292F63;font-family: Campton;">{{__("More Info")}}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
+                        </div><a href="ourschools/medan" wire:navigate.hover style="color: #292F63;font-family: Campton;">{{__("More Info")}}&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2929 5.29289C12.6834 4.90237 13.3166 4.90237 13.7071 5.29289L17.7071 9.29289C18.0976 9.68342 18.0976 10.3166 17.7071 10.7071L13.7071 14.7071C13.3166 15.0976 12.6834 15.0976 12.2929 14.7071C11.9024 14.3166 11.9024 13.6834 12.2929 13.2929L14.5858 11H3C2.44772 11 2 10.5523 2 10C2 9.44772 2.44772 9 3 9H14.5858L12.2929 6.70711C11.9024 6.31658 11.9024 5.68342 12.2929 5.29289Z" fill="currentColor"></path>
                             </svg></a>
                     </div>
