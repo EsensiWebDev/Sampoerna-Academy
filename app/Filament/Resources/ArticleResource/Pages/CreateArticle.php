ba094525c646adf4f6cf6ad36571870f169625a5
feriwnarta26 <feri@suiten.com>
2024-11-13 11:43:49 +0700

    update

--- app/Filament/Resources/ArticleResource/Pages/CreateArticle.php
+++ app/Filament/Resources/ArticleResource/Pages/CreateArticle.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\ArticleResource\Pages;
 use App\Filament\Resources\ArticleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Log;
 
 class CreateArticle extends CreateRecord
 {
@@ -13,6 +14,8 @@ class CreateArticle extends CreateRecord
     protected function afterCreate()
     {
         $article = $this->record; // Menangkap model article yang baru dibuat
+
+        Log::info($article);
 
         if ($article) {
             \SEO\Seo::save($article, route('read-article', $article->slugs), [
