11189c51e54b8712d4088203953ccb09cb3fcd4b
feriwnarta26 <feri@suiten.com>
2024-11-15 10:25:18 +0700

    update fix

--- resources/views/toolbox.blade.php
+++ resources/views/toolbox.blade.php
@@ -1,8 +1,16 @@
-<div class="d-flex flex-column" style="position: fixed; top: 90%; right: 20px; transform: translateY(-50%);">
+<div class="d-flex flex-column"
+     style="
+     position: fixed; top: 90%; right: 0px; transform: translateY(-50%);
+     background-color: white;
+     width: 57px;
+     border-radius: 25px 0 0 25px;
+     ">
+    <div style="margin-left: 13px; margin-top: 23px; margin-bottom: 23px;">
     <a href="/admissions/how-to-apply">
         <img src="{{ asset('assets/img/note.webp') }}" alt="Note" style="margin-bottom: 33px; width: 40px; height: auto;">
     </a>
     <a href="">
         <img src="{{ asset('assets/img/wa.webp') }}" alt="Wa" style="width: 40px; height: auto;">
     </a>
+    </div>
 </div>
