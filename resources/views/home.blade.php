ee1c114fc6d9e82eb3333fe7da1e594b1e7d742f
feriwnarta26 <feri@suiten.com>
2024-11-18 02:08:26 +0700

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="icon" type="image/webp" sizes="1131x1345" href="{{ asset("assets/img/Icon/Five%20Icon.webp") }}">
     <link rel="stylesheet" href="{{ asset("assets/bootstrap/css/bootstrap.min.css") }}">
-    <link rel="stylesheet" href="{{ asset("assets/css/styles.min.css") }}">
-    <link rel="stylesheet" href="{{ asset("assets/css/style.css") }}">
-    <link rel="stylesheet" href="{{ asset("assets/slick-1.8.1/slick/slick.css") }}">
-    <link rel="stylesheet" href="{{ asset("assets/slick-1.8.1/slick/slick-theme.css") }}">
+    <link rel="stylesheet" href="{{ asset("assets/css/styles.min.css") }}" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="{{ asset("assets/css/style.css") }}" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="{{ asset("assets/slick-1.8.1/slick/slick.css") }}" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="{{ asset("assets/slick-1.8.1/slick/slick-theme.css") }}" media="print" onload="this.media='all'">
     <script src="{{ asset("assets/js/jquery.js") }}" defer></script>
     <title>{{ config("app.name") }}</title>
     @seoTags()
