602c1188096cb1bbe3a379be25abce853a13004a
Hecky Riadi <heckyy@192.168.1.15>
2024-11-21 14:08:55 +0700

    Create feature Form Handling

---
+++ app/Models/Lead.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lead extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'leads';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'parents_name',
+        'kids_name',
+        'phone_number',
+        'email',
+        'province',
+        'prev_curriculum',
+        'school_choice',
+        'grade_interested',
+        'timeframe_visit',
+    ];
+}
