aa09530ff65013ebc17d109b623a6c586dd120ce
crevasse02 <wiboworaffi82@gmail.com>
2024-11-28 11:25:19 +0700

    update form contact

--- resources/views/livewire/pages/contact.blade.php
+++ resources/views/livewire/pages/contact.blade.php
@@ -26,90 +26,104 @@
                         style="border-radius: 15px;border-style: none;box-shadow: 0px 10px 20px rgba(96,97,112,0.16), 0px 2px 5px rgba(40,41,61,0.4); margin-top: 61px">
                         <div class="card-body"
                             style="padding-left: 40px;padding-right: 40px;padding-top: 40px;padding-bottom: 40px;">
-                            <form method="POST" action="/form">
-                                <div style="margin-bottom: 40px;"><label class="form-label"
-                                        style="color: var(--bs-black);font-family: Campton;">{{ __('Parents Name') }}<span
-                                            class="text-danger">*</span></label><input class="form-control"
-                                        type="text"></div>
-                                <div style="margin-bottom: 40px;"><label class="form-label"
-                                        style="color: var(--bs-black);font-family: Campton;">{{ __('Kids Name') }}<span
-                                            class="text-danger">*</span></label><input class="form-control"
-                                        type="text"></div>
-                                <div class="d-flex flex-row" style="margin-bottom: 40px;">
-                                    <div class="flex-fill" style="padding-right: 10px;"><label class="form-label"
-                                            style="color: var(--bs-black);font-family: Campton;">{{ __('Phone Number') }}<span
-                                                class="text-danger">*</span></label><input class="form-control"
-                                            type="tel"></div>
-                                    <div class="flex-fill" style="padding-left: 10px;"><label class="form-label"
-                                            style="color: var(--bs-black);font-family: Campton;">Email<span
-                                                class="text-danger">*</span></label><input class="form-control"
-                                            type="email"></div>
+                            <form action="/form" method="POST">
+                                @csrf
+                                {{-- Tampilkan pesan error --}}
+                                <div class="mb-4">
+                                    <label class="form-label"
+                                        style="color: var(--bs-black); font-family: Campton;">{{ __('Parents Name') }}
+                                        <span class="text-danger">*</span></label>
+                                    <input class="form-control" name="parents_name" type="text" required>
                                 </div>
-                                <div style="margin-bottom: 40px;"><label class="form-label"
-                                        style="color: var(--bs-black);font-family: Campton;">{{ __('Province') }}<span
-                                            class="text-danger">*</span></label><select class="form-select ">
-                                        <optgroup label="Province">
-                                            <option value="0" selected="">{{ __('Choose one') }}</option>
-                                            @foreach ($provinces as $province)
-                                                <option value="{{ $province['id'] }}">{{ $province['name'] }}</option>
-                                            @endforeach
-                                        </optgroup>
-                                    </select></div>
-                                {{-- <div style="margin-bottom: 40px;"><label class="form-label"
-                                        style="color: var(--bs-black);font-family: Campton;">{{ __('Previous Curriculume') }}<span
-                                            class="text-danger">*</span></label><select class="form-select">
-                                        <optgroup label="Province">
-                                            <option value="12" selected="">{{ __('Choose one') }}</option>
-                                            <option value="13">This is item 2</option>
-                                            <option value="14">This is item 3</option>
-                                        </optgroup>
-                                    </select></div> --}}
-                                <div style="margin-bottom: 40px;"><label class="form-label"
-                                        style="color: var(--bs-black);font-family: Campton;">{{ __('School Choice') }}<span
-                                            class="text-danger">*</span></label><select class="form-select">
-                                        <optgroup label="School Choice">
-                                            <option value="12" selected="">{{ __('Choose one') }}</option>
-                                            <option value="13">{{ __('L’Avenue School, Jakarta') }}</option>
-                                            <option value="14">{{ __('BSD School, South Tangerang') }}</option>
-                                            <option value="">{{ __('Sentul School, West Java') }}</option>
-                                            <option value="">
-                                                {{ __('Pakuwon Indah & Grand Pakuwon School, Surabaya') }}</option>
-                                            <option value="">{{ __('Cipto & Citra School, Medan') }}</option>
-                                        </optgroup>
-                                    </select></div>
-                                <div style="margin-bottom: 40px;"><label class="form-label"
-                                        style="color: var(--bs-black);font-family: Campton;">{{ __('Grade Interested') }}</label><span
-                                        class="text-danger">*</span><select class="form-select">
-                                        <optgroup label="Grade Interested">
-                                            <option value="12">{{ __('Choose one') }}</option>
-                                            <option value="">{{ __('Early Years') }}</option>
-                                            <option value="">{{ __('Elementary School') }}</option>
-                                            <option value="">{{ __('Middle School') }}</option>
-                                            <option value="">{{ __('High School') }}</option>
-                                            <option value="">{{ __('Online School') }}</option>
-                                        </optgroup>
-                                    </select></div>
-                                <div style="margin-bottom: 40px;"><label class="form-label"
-                                        style="color: var(--bs-black);font-family: Campton;">{{ __('Timeframe Visit') }}<span
-                                            class="text-danger">*</span></label><select class="form-select">
-                                        <optgroup label="Timeframe Visit">
-                                            <option value="12" selected="">{{ __('Choose one') }}</option>
-                                            <option value="">{{ __('Within This Week') }}</option>
-                                            <option value="">{{ __('This Month') }}</option>
-                                            <option value="">{{ __('3 Months') }}</option>
-                                        </optgroup>
-                                    </select></div>
-                                @if ($errors->has('g-recaptcha-response'))
+                                <div class="mb-4">
+                                    <label class="form-label"
+                                        style="color: var(--bs-black); font-family: Campton;">{{ __('Kids Name') }}
+                                        <span class="text-danger">*</span></label>
+                                    <input class="form-control" name="kids_name" type="text" required>
+                                </div>
+                                <div class="d-flex flex-wrap mb-4">
+                                    <div class="flex-fill me-2">
+                                        <label class="form-label"
+                                            style="color: var(--bs-black); font-family: Campton;">{{ __('Phone Number') }}
+                                            <span class="text-danger">*</span></label>
+                                        <input class="form-control" name="phone_number" type="tel" required>
+                                    </div>
+                                    <div class="flex-fill ms-2">
+                                        <label class="form-label"
+                                            style="color: var(--bs-black); font-family: Campton;">Email
+                                            <span class="text-danger">*</span></label>
+                                        <input class="form-control" name="email" type="email" required>
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label"
+                                        style="color: var(--bs-black); font-family: Campton;">{{ __('Province') }}
+                                        <span class="text-danger">*</span></label>
+                                    <select class="form-select" name="province" required>
+                                        <option value="" selected>{{ __('Choose one') }}</option>
+                                        @foreach ($provinces as $province)
+                                            <option value="{{ $province['id'] }}">{{ $province['name'] }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                {{-- <div class="mb-4">
+                                    <label class="form-label"
+                                        style="color: var(--bs-black); font-family: Campton;">{{ __('Previous Curriculum') }}
+                                        <span class="text-danger">*</span></label>
+                                    <select class="form-select" name="prev_curriculum" required>
+                                        <option value="" selected>{{ __('Choose one') }}</option>
+                                        <option value="item2">This is item 2</option>
+                                        <option value="item3">This is item 3</option>
+                                    </select>
+                                </div> --}}
+                                <div class="mb-4">
+                                    <label class="form-label"
+                                        style="color: var(--bs-black); font-family: Campton;">{{ __('School Choice') }}
+                                        <span class="text-danger">*</span></label>
+                                    <select name="school_choice" class="form-select" required>
+                                        <option value="" selected>{{ __('Choose one') }}</option>
+                                        <option value="L’Avenue School">{{ __('L’Avenue School, Jakarta') }}</option>
+                                        <option value="BSD School">{{ __('BSD School, South Tangerang') }}</option>
+                                        <option value="Sentul School">{{ __('Sentul School, West Java') }}</option>
+                                        <option value="Pakuwon Indah School">
+                                            {{ __('Pakuwon Indah & Grand Pakuwon School, Surabaya') }}</option>
+                                        <option value="Cipto School">{{ __('Cipto & Citra School, Medan') }}</option>
+                                    </select>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label"
+                                        style="color: var(--bs-black); font-family: Campton;">{{ __('Grade Interested') }}</label>
+                                    <span class="text-danger">*</span>
+                                    <select class="form-select" name="grade_interested" required>
+                                        <option value="">{{ __('Choose one') }}</option>
+                                        <option value="Early Years">{{ __('Early Years') }}</option>
+                                        <option value="Elementary School">{{ __('Elementary School') }}</option>
+                                        <option value="Middle School">{{ __('Middle School') }}</option>
+                                        <option value="High School">{{ __('High School') }}</option>
+                                        <option value="Online School">{{ __('Online School') }}</option>
+                                    </select>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label"
+                                        style="color: var(--bs-black); font-family: Campton;">{{ __('Timeframe Visit') }}
+                                        <span class="text-danger">*</span></label>
+                                    <select class="form-select" name="timeframe_visit" required>
+                                        <option value="" selected>{{ __('Choose one') }}</option>
+                                        <option value="Within This Week">{{ __('Within This Week') }}</option>
+                                        <option value="This Month">{{ __('This Month') }}</option>
+                                        <option value="3 Months">{{ __('3 Months') }}</option>
+                                    </select>
+                                </div>
+                                {{-- @if ($errors->has('g-recaptcha-response'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
                                     </span>
-                                @endif
-                                {!! \Anhskohbo\NoCaptcha\Facades\NoCaptcha::renderJs() !!}
-                                {!! \Anhskohbo\NoCaptcha\Facades\NoCaptcha::display() !!}
-
-                                <div class="d-lg-flex justify-content-lg-end"><button class="btn btn-primary"
-                                        type="button"
-                                        style="border-style: none;border-radius: 12px;padding-left: 40px;padding-right: 40px;padding-top: 20px;padding-bottom: 20px;font-family: Campton;width: 320px;">{{ __('Submit') }}</button>
+                                @endif --}}
+                                {{-- {!! \Anhskohbo\NoCaptcha\Facades\NoCaptcha::renderJs() !!} --}}
+                                {{-- {!! \Anhskohbo\NoCaptcha\Facades\NoCaptcha::display() !!} --}}
+                                <div class="d-flex justify-content-end">
+                                    <button class="btn btn-primary" type="submit"
+                                        style="border-style: none; border-radius: 12px; padding: 20px 40px; font-family: Campton; max-width: 320px;">{{ __('Submit') }}</button>
                                 </div>
                             </form>
                         </div>
