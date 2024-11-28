2cb0caa9e846470f8d0c8053baeef02c4ef5fe5c
crevasse02 <wiboworaffi82@gmail.com>
2024-11-28 17:23:05 +0700

    update recent post

--- app/Livewire/Pages/DetailArticle.php
+++ app/Livewire/Pages/DetailArticle.php
@@ -11,9 +11,15 @@ class DetailArticle extends Component
 
     public function render()
     {
+        // Get the current article by its slug
         $article = Article::where('slug', $this->slug)->first();
 
-        return view('livewire.pages.detail-article', compact('article'));
+        // Get 3 random articles excluding the current article based on its slug
+        $articles = Article::where('slug', '!=', $this->slug)
+            ->inRandomOrder()  // Randomize the result
+            ->paginate(3);     // Paginate the result to 3 articles per page
+
+        return view('livewire.pages.detail-article', compact('article', 'articles'));
     }
 
     public function detailPage(string $slug)
@@ -21,3 +27,4 @@ class DetailArticle extends Component
         $this->redirect("/articles/{$slug}");
     }
 }
+
--- resources/views/livewire/pages/detail-article.blade.php
+++ resources/views/livewire/pages/detail-article.blade.php
@@ -5,11 +5,9 @@
         </div>
     @else
         <section>
+
             <div class="d-flex align-items-end"
-                style="height: 600px;
-        background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 100%),
-        url('{{ asset($article->thumbnail) }}') center / cover no-repeat,
-        linear-gradient(to top, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 100%);">
+                style="height: 600px; background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 100%),url('{{ asset($article->thumbnail) }}') center / cover no-repeat,linear-gradient(to top, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 100%);">
 
                 <div class="container" style="padding-bottom: 64px;">
                     <div class="row">
@@ -50,8 +48,76 @@
     @endif
 
 
-    <section id="content" class="px-4 py-4 mx-4">
-        {!! $content !!}
+    <section id="content" class="px-md-4 py-4 mx-4">
+        <div class="row px-md-4 py-md-5">
+            <div class="col-md-9 col-sm-12 pe-md-5">
+                {!! $content !!}
+            </div>
+            <div class="col-md-3 col-sm-12" >
+                <h3 style="color:#8a8a8a">Recent Post</h3>
+                <div class="d-flex flex-column">
+                    @if (isset($articles) && $articles->count() > 1)
+                        @foreach ($articles as $article)
+                            <div class="mt-4 rounded" style="background-color: #292F78;"
+                                wire:key="{{ $loop->iteration }}">
+                                <a href="/news/{{ $article->slug }}" style="color: black">
+                                    <div>
+
+                                        <img class="img-fluid rounded-top"
+                                            style="margin-bottom: 36px; height: 204px; width: 100%; object-fit: cover;"
+                                            src="{{ asset($article->thumbnail) }}" alt="Article Thumbnail">
+                                    </div>
+
+                                    @php
+                                        $locale = app()->getLocale();
+                                        $title =
+                                            $locale == 'id'
+                                                ? $article->title_indonesia ?? $article->title_english
+                                                : $article->title_english ?? $article->title_indonesia;
+                                        $content =
+                                            $locale == 'id'
+                                                ? \Illuminate\Support\Str::limit(
+                                                    preg_replace(
+                                                        '/$$[^$$]*$$/',
+                                                        '',
+                                                        strip_tags($article->content_indonesia),
+                                                    ),
+                                                    100,
+                                                    '...',
+                                                )
+                                                : \Illuminate\Support\Str::limit(
+                                                    preg_replace(
+                                                        '/$$[^$$]*$$/',
+                                                        '',
+                                                        strip_tags($article->content_english),
+                                                    ),
+                                                    100,
+                                                    '...',
+                                                );
+                                    @endphp
+
+                                    <div class="px-4">
+
+                                        <p class="fw-light" style="font-family: Campton; color: white;">
+                                            {{ \Carbon\Carbon::parse($article->created_at)->format('F j, Y') }}
+                                        </p>
+                                        <h1 class="fs-5 fw-semibold"
+                                            style="color: var(--bs-white); font-family: Campton; margin-top: 8px; margin-bottom: 8px;">
+                                            {!! $title !!}
+                                        </h1>
+                                        <p class=" fw-light"
+                                            style="color: var(--bs-white); font-family: Campton; word-wrap: break-word;">
+                                            {!! $content !!}
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+
+        </div>
     </section>
     @include('components.form')
 
