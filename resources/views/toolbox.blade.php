fa10ef7e6e8da3de339c613c39a8e4f26c05ad16
crevasse02 <wiboworaffi82@gmail.com>
2025-01-09 17:08:40 +0700

    Update link wa

--- resources/views/toolbox.blade.php
+++ resources/views/toolbox.blade.php
@@ -10,7 +10,7 @@
     <a href="/admissions/how-to-apply">
         <img src="{{ asset('assets/img/note.webp') }}" width="40" height="40" alt="Note" style="margin-bottom: 33px; width: 40px; height: auto;">
     </a>
-    <a href="">
+    <a href="https://wa.link/52fwu8" target="_blank">
         <img src="{{ asset('assets/img/wa.webp') }}" width="40" height="40" alt="Wa" style="width: 40px; height: auto;">
     </a>
     </div>
